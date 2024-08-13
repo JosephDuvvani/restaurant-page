84c900bfeb38bc2c1e1977adcd3fcf655e26346b
JosephDuvvani <josephduvvani@gmail.com>
2024-08-13 22:55:42 +0200

    Add food images and 'Order Now' button

--- src/home.js
+++ src/home.js
@@ -1,4 +1,8 @@
 import mainImagePath from './plates.jpg';
+import foodImagePath1 from './food-img-1.jpg';
+import foodImagePath2 from './food-img-2.jpg';
+import foodImagePath3 from './food-img-3.jpg';
+import foodImagePath4 from './food-img-4.jpg';
 import './style.css';
 import './home.css';
 
@@ -30,12 +34,36 @@ export default function () {
     mainTitle.appendChild(mainImage);
     contentDiv.appendChild(mainTitle);
 
+    const foodGallery = document.createElement('div');
+    foodGallery.classList.add('mini-food-gallery', 'display-grid');
+
     const showcaseFood = document.createElement('div');
     showcaseFood.classList.add('showcase-food', 'display-flex', 'center-items');
     for(let i = 0; i < 4; i++) {
         const foodImage = document.createElement('div');
         foodImage.classList.add('food-image');
+
+        const paths = [
+            foodImagePath1,
+            foodImagePath2,
+            foodImagePath3,
+            foodImagePath4
+        ];
+        const image = document.createElement('img');
+        image.src = paths[i];
+        image.classList.add(`food-img-${i+1}`, 'food-img');
+        image.setAttribute('alt', `Mini Gallery Image ${i+1}`);
+
+        foodImage.appendChild(image);
         showcaseFood.appendChild(foodImage);
     }
-    contentDiv.appendChild(showcaseFood);
+
+    const orderNowBtn = document.createElement('button');
+    orderNowBtn.classList.add('order-now-btn', 'primary-btn-colors', 'btn-border');
+    orderNowBtn.setAttribute('type', 'button');
+    orderNowBtn.textContent = 'ORDER NOW';
+
+    foodGallery.appendChild(showcaseFood);
+    foodGallery.appendChild(orderNowBtn);
+    contentDiv.appendChild(foodGallery);
 }
